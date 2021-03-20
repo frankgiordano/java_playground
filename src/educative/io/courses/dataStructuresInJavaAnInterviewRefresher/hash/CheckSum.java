ac63e89f0e7e7e4c190da80e12d8250bc63a0b6a
Frank Giordano <giofr01@yahoo.com>
2021-03-20 15:52:08 -0400

    Hash Chapter - Challenge 10 - CheckSum added another solution that uses one loop only

--- src/educative/io/courses/dataStructuresInJavaAnInterviewRefresher/hash/CheckSum.java
+++ src/educative/io/courses/dataStructuresInJavaAnInterviewRefresher/hash/CheckSum.java
@@ -43,11 +43,38 @@ public class CheckSum {
         return result;
     }
 
+    public static int[] findSum2(int[] arr, int n) {
+        // solution with one loop..
+        if (arr == null)
+            return null;
+
+        int[] result = new int[2];
+        Set<Integer> values = new HashSet<>();
+
+        // O(n)
+        for (int i = 0; i < arr.length; i++) {
+            int difference = n - arr[i];
+            if (values.contains(difference)) {
+                result[0] = arr[i];
+                result[1] = difference;
+                return result;
+            } else {
+                values.add(arr[i]);
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4,78, 99, 98, 2 };
         arr = CheckSum.findSum(arr, 100);
         for (int i = 0; i < arr.length; i++)
             System.out.println(arr[i]);
+        int[] arr2 = { 1, 2, 3, 4,78, 99, 98, 2 };
+        arr2 = CheckSum.findSum2(arr2, 100);
+        for (int i = 0; i < arr2.length; i++)
+            System.out.println(arr2[i]);
     }
 
 
