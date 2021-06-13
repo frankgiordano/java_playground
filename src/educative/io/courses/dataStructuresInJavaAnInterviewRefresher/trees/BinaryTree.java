2486c071129bff5fc7440fa940e4d959edf3b21b
Frank Giordano <giofr01@yahoo.com>
2021-06-12 20:08:47 -0400

    filling out btree functionality

--- src/educative/io/courses/dataStructuresInJavaAnInterviewRefresher/trees/BinaryTree.java
+++ src/educative/io/courses/dataStructuresInJavaAnInterviewRefresher/trees/BinaryTree.java
@@ -1,10 +1,17 @@
 package educative.io.courses.dataStructuresInJavaAnInterviewRefresher.trees;
-
 public class BinaryTree {
 
     private Node root;
 
     public BinaryTree() {
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
     }
 
     public boolean insert(int value) {
@@ -93,8 +100,158 @@ public class BinaryTree {
         }
     }
 
-    private boolean isEmpty() {
-        return this.root == null;
+    public boolean delete(int value) {
+        return delete(value, this.root);
+    }
+
+    private boolean delete(int value, Node currNode) {
+        if (isEmpty())
+            return false;
+
+        Node parent = null;  // to store parent of currNode
+        while (currNode != null && (currNode.getData() != value)) {
+            parent = currNode;
+            if (value > currNode.getData())
+                currNode = currNode.getRightChild();
+            else
+                currNode = currNode.getLeftChild();
+        }
+
+        if (currNode == null) {
+            return false;
+        } else if (currNode.getLeftChild() == null && currNode.getRightChild() == null) {
+            // 1. Node is a Leaf Node
+            // if that leaf node is the root (a tree with just a root)
+            if (root.getData() == currNode.getData()) {
+                setRoot(null);
+                return true;
+            } else if (currNode.getData() < parent.getData()) {
+                parent.setLeftChild(null);
+                return true;
+            } else {
+                parent.setRightChild(null);
+                return true;
+            }
+        } else if (currNode.getRightChild() == null) {
+            if (root.getData() == currNode.getData()) {
+                setRoot(currNode.getLeftChild());
+                return true;
+            } else if (currNode.getData() < parent.getData()) {
+                parent.setLeftChild(currNode.getLeftChild());
+                return true;
+            } else {
+                parent.setRightChild(currNode.getLeftChild());
+                return true;
+            }
+        } else if (currNode.getLeftChild() == null) {
+            if (root.getData() == currNode.getData()) {
+                setRoot(currNode.getRightChild());
+                return true;
+            } else if (currNode.getData() < parent.getData()) {
+                parent.setLeftChild(currNode.getRightChild());
+                return true;
+            } else {
+                parent.setRightChild(currNode.getRightChild());
+                return true;
+            }
+        } else {
+            //Find Least Value Node in right-subtree of current Node
+            Node leastNode = findLeastNode(currNode.getRightChild());
+            //Set CurrentNode's Data to the least value in its right-subtree
+            int temp = leastNode.getData();
+            delete(temp, root);
+            currNode.setData(temp);
+            //Delete the leafNode which had the least value
+            return true;
+        }
+
+    }
+
+    // Helper function to find least value node in right-subtree of currentNode
+    private Node findLeastNode(Node currNode) {
+        Node temp = currNode;
+
+        while (temp.getLeftChild() != null) {
+            temp = temp.getLeftChild();
+        }
+
+        return temp;
+    }
+
+    //Iterative Function to insert a value in BST
+    public boolean add(int value) {
+
+        //If Tree is empty then insert Root with the given value inside Tree
+        if (isEmpty()) {
+            root = new Node(value);
+            return true;
+        }
+
+        //Starting from root
+        Node currNode = root;
+
+        //Traversing the tree untill valid position to insert the value
+        while (currNode != null) {
+
+            Node leftChild = currNode.getLeftChild();
+            Node rightChild = currNode.getRightChild();
+
+            //If the value to insert is less than root value
+            //then move to left subtree else move to right subtree of root
+            //and before moving check if the subtree is null, if it's then insert the value.
+
+            if (currNode.getData() > value) {
+                if (leftChild == null) {
+                    leftChild = new Node(value);
+                    currNode.setLeftChild(leftChild);
+                    return true;
+                }
+                currNode = leftChild;
+            } else {
+                if (rightChild == null) {
+                    rightChild = new Node(value);
+                    currNode.setRightChild(rightChild);
+                    return true;
+                }
+                currNode = rightChild;
+            } //end of else
+        } //end of while
+        return false;
+    }
+
+    //Function to check if Tree is empty or not
+    public boolean isEmpty() {
+        return root == null; //if root is null then it means Tree is empty
+    }
+
+    //Just for Testing purpose - pre-order Traversal
+    public void printTree(Node currNode) {
+        if (currNode == null)
+            return;
+
+        System.out.print(currNode.getData() + ",");
+        printTree(currNode.getLeftChild());
+        printTree(currNode.getRightChild());
+    }
+
+    public void inTraverse(Node root) {
+        if (root == null)
+            return;
+
+        inTraverse(root.getLeftChild());
+        System.out.print(root.getData() + ",");
+        inTraverse(root.getRightChild());
+
+    }
+
+    public void postTraverse(Node root) {
+        if (root == null)
+            return;
+
+        postTraverse(root.getLeftChild());
+        postTraverse(root.getRightChild());
+        System.out.print(root.getData() + ",");
+
     }
 
 }
--- src/educative/io/courses/dataStructuresInJavaAnInterviewRefresher/trees/Tests.java
+++ src/educative/io/courses/dataStructuresInJavaAnInterviewRefresher/trees/Tests.java
@@ -5,18 +5,23 @@ public class Tests {
     public static void main(String[] args) {
 
         BinaryTree btree = new BinaryTree();
-        btree.insert(1);
-        btree.insert(2);
-        btree.insert(3);
+        btree.insert(6);
         btree.insert(4);
+        btree.insert(2);
         btree.insert(5);
-        btree.insert(10);
+        btree.insert(9);
         btree.insert(8);
+        btree.insert(12);
 
         System.out.println("Found 2 " +
                 (btree.search(2) != null ? (btree.search(2).getData() == 2) : "false"));
         System.out.println("Found 21 " +
                 (btree.search(21) != null ? (btree.search(21).getData() == 21) : "false"));
+        btree.printTree(btree.getRoot());
+        System.out.println();
+        btree.inTraverse(btree.getRoot());
+        System.out.println();
+        btree.postTraverse(btree.getRoot());
     }
 
 }
