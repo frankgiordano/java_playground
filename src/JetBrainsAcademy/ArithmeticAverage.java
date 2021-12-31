59f1759ec2751ad2111899a7ef2c491b1258a40c
Frank Giordano <giofr01@yahoo.com>
2021-12-30 19:55:12 -0500

    tweak

--- src/JetBrainsAcademy/ArithmeticAverage.java
+++ src/JetBrainsAcademy/ArithmeticAverage.java
@@ -7,16 +7,16 @@ import java.util.List;
 /**
  * Write a program that reads two numbers aa and bb from the keyboard and calculates and outputs to the
  * console the arithmetic average of all numbers from the interval [a; b][a;b], which are divisible by 33.
- * <p>
+ *
  * In the example below, the arithmetic average is calculated for the numbers on the interval [-5; 12][−5;12].
  * On this interval, there are 66 numbers divisible by 33, namely: -3, 0, 3, 6, 9, 12−3,0,3,6,9,12. Their
  * arithmetic average equals 4.54.5.
- * <p>
+ *
  * The program input contains intervals, which always contain at least one number, which is divisible by 33.
- * <p>
+ *
  * Remember that the int type cannot contain fractions. Use a double variable to store the precise result
  * of the division.
- * <p>
+ * 
  * author: frank giordano
  */
 public class ArithmeticAverage {
